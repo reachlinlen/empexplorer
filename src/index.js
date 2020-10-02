098f2772bb9abdf75958383e4b8048eb7116416c
Nirumam <reachlinlen@gmail.com>
2020-10-02 08:15:48 +0800

    code improvement

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
-import Home from '../src/Pages/Home'
+import Home from './Pages/Home'
 import Overview from './Pages/Overview'
 
 import './assets/main.css'
 
 ReactDOM.render(
   <BrowserRouter>
-    {/* <Routes>
-      <Home path="/" />
-      <Overview path="overview/:name" />
-    </Routes> */}
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/overview/:name" component={Overview} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("container"))
+
+if (module.hot) {
+  module.hot.accept();
+}
